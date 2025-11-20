c4f69c1b337e6ee6c932b847ce6ed7922919fbb3
Hussain Wael <hussain.h.ff32@gmail.com>
2025-11-20 13:09:46 +0000

    Request URL http://localhost:9002/ Request Method GET Status Code 500 In

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv';
 config();
 
+// Import your flows here
+// e.g. import './flows/my-flow.ts';
 import '@/ai/flows/generate-faq-answers.ts';
 import '@/ai/flows/chatbot-flow.ts';
